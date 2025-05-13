e3c0f95cb9ba8eb01abe41eba8edc514aab44299
Nathalie Cedeño Rodriguez <148410296+NathalieCedeno@users.noreply.github.com>
2025-05-13 12:18:31 -0500

    Update Product.java

--- src/main/java/com/inventory/system/model/Product.java
+++ src/main/java/com/inventory/system/model/Product.java
@@ -9,7 +9,7 @@ package com.inventory.system.model;
 public class Product {
     
     /**
-        * Identificador del producto.
+        * Identificador único del producto.
     */
     private int id;
     
